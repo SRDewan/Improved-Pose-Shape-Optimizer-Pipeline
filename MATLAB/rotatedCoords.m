4f2f23bbb8a28770c3a945c53bb5130f092f271e
SRDewan <shaurya.dewan@students.iiit.ac.in>
2021-06-13 17:35:38 +0530

    Improved scaling documentation. Also done with rotation to camera coords.

--- MATLAB/rotatedCoords.m
+++ MATLAB/rotatedCoords.m
@@ -1,4 +1,27 @@
-function [] = rotatedCoords()
+function [camWireframe, camBasisVecs] = rotatedCoords()
 
 wireframe = scalingFactor();
-visualizeWireframe3D(wireframe);
+% visualizeWireframe3D(wireframe);
+
+% As per the camera coordinate system:
+% The X axis is along the width of the car from left to right (left
+% headlight to right headlight).
+% The Y axis is along the height of the car from top to bottom (roof top to wheel).
+% The Z axis is along the length of the car from back (tail light to
+% headlight) to front.
+
+% We compare this with the world coordinate system to obtain the required
+% rotation matrix. The negative sign indicates a 180 turn in direction.
+R = [[-1, 0, 0]', [0, 0, -1]', [0, -1, 0]'];
+camWireframe = R * wireframe;
+
+basisVecs = load('../parameters/vectors.txt');
+camBasisVecs = [];
+
+for i = 1:size(basisVecs, 1)
+    temp = reshape(basisVecs(i, :), [3, 14]);
+    temp = R * temp;
+    camBasisVecs(i, :) = reshape(temp, [1, 42]);
+end
+
+% disp(camBasisVecs);
--- MATLAB/scalingFactor.m
+++ MATLAB/scalingFactor.m
@@ -2,15 +2,19 @@ function [scaledWireframe] = scalingFactor()
 
 wireframe = load('../parameters/meanShape.txt');
 wireframe = wireframe';
+visualizeWireframe3D(wireframe);
 
 % 1st column vector is for width which is along the X axis best estimated
-% along the axis formed by L_F_WheelCenter and R_F_WheelCenter
+% along the axis formed by L_F_WheelCenter and R_F_WheelCenter. The X axis
+% is from the right to the left of the car.
 % 2nd column vector is for length which is along the Y axis best estimated
-% along the axis formed by L_TailLight and L_HeadLight
+% along the axis formed by L_TailLight and L_HeadLight. The Y axis is from
+% the front to the back of the car.
 % 3rd column vector is for height which is along the Z axis best estimated
-% along the axis formed by R_B_RoofTop and R_B_WheelCenter
+% along the axis formed by R_B_RoofTop and R_B_WheelCenter. The Z axis is
+% from the bottom to the top of the car.
 
-Axes = [[wireframe(:, 2) - wireframe(:, 1)], [wireframe(:, 7) - wireframe(:, 5)], [wireframe(:, 14) - wireframe(:, 4)]];
+Axes = [[wireframe(:, 1) - wireframe(:, 2)], [wireframe(:, 7) - wireframe(:, 5)], [wireframe(:, 14) - wireframe(:, 4)]];
 % Length, width and height values are estimated by taking the norm of the above axes vectors
 dims = vecnorm(Axes);
 % The axes are supposed to be unit vectors and hence the division by the norm
